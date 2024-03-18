2d8b42d3f22682eabd8085f6ac8bec04e268fce6
Tharun Kasireddy <tharunkasireddyde@gmail.com>
2024-03-18 17:55:05 -0500

    Changed list

--- list_sample.py
+++ list_sample.py
@@ -16,5 +16,5 @@ def even_or_odd(input_data):
             result = False
             output_data.append(result)
     return output_data
-a = [1,2,3,4,5,6,7,8,9,11]
+a = [1,2,3,4,5,6,7,8,9,10]
 print(even_or_odd(a))
