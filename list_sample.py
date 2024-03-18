af968ccf8dd13d0ac2ee4e5c178141aa0a642099
Tharun Kasireddy <tharunkasireddyde@gmail.com>
2024-03-18 17:49:24 -0500

    added list samples

---
+++ list_sample.py
@@ -0,0 +1,20 @@
+
+#create a function to return a list having true for even numbers false for odd numbers for a given list
+
+# create function
+# list is input having numbers
+# give conditions to get result
+# if remaimder is 0 then true else false
+
+def even_or_odd(input_data):
+    output_data = []
+    for number in input_data:
+        if number%2 == 0:
+            result = True
+            output_data.append(result)
+        else:
+            result = False
+            output_data.append(result)
+    return output_data
+a = [1,2,3,4,5,6,7,8,9,11]
+print(even_or_odd(a))
